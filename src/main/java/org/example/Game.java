c90f048e5ebaa2c8295c3f2c041eac3504b38661
bhuvaneshwari-k <bhuvaneshwari.k@tringapps.com>
2023-03-03 16:59:04 +0530

    commit

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -9,7 +9,6 @@ public class Game{
         Scanner s=new Scanner(System.in);
         Scanner s1=new Scanner(System.in);
         PrintStream l=new PrintStream((new FileOutputStream(FileDescriptor.out)));
-//        Player p=new Player();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 GameBoard.board[i][j] = ' ';
@@ -23,25 +22,16 @@ public class Game{
         String name2=s.nextLine();
         l.println("Enter the player2 key:");
         char key2=s1.next().charAt(0);
-        l.println("1.Player1\n2.Player2");
-        l.println("Enter the choice that which player you want to play first:");
-        int ch= s1.nextInt();
         int count1=1;
-        if(ch==1) {
             while(count1<10) {
-                if(count1!=10)
-                   count1=GameBoard.player1(name1,key1,count1);
-                if(count1!=10)
-                    count1=GameBoard.player2(name2,key2,count1);
-            }
-        }
-        else {
-            while(count1<10) {
-                if(count1!=10)
-                    count1=GameBoard.player2(name2,key2,count1);
-                if(count1!=10)
-                    count1=GameBoard.player1(name1,key1,count1);
+                if(count1%2!=0) {
+                    count1 = GameBoard.player1(name1, key1, count1);
+                    count1++;
+                }
+                if(count1%2==0) {
+                    count1 = GameBoard.player2(name2, key2, count1);
+                    count1++;
+                }
             }
         }
     }
-}
